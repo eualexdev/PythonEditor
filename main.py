693af3820394e7d0146324d28b65ccaf427ab0e1
José Alexsandro <68931503+eualexdev@users.noreply.github.com>
2021-05-04 20:26:50 -0300

    Mudei algumas coisas

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from loads import ExecuteFunctionsOfTime
 
-from install import Install
-
 from src.splashscreen import SplashScreen
 
 
 def Main() -> None :
-    Install() # Instalador
-
     App = QApplication([])
     SplashUi = SplashScreen()    
     SplashUi.show()
