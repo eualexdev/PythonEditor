072dadcfd97d2216299d9003f36641371375a097
José Alexsandro <68931503+eualexdev@users.noreply.github.com>
2021-05-04 20:25:46 -0300

    Delete install.py

--- install.py
+++
@@ -1,8 +0,0 @@
-from core.engine import getPlatform
-from core.windows import WindowsClass
-
-platform_os = getPlatform()
-def Install():
-    global platform_os
-    if platform_os == "win":
-        WindowsClass.Install()
