5ba59da28591104cd71185d15870f810f441e6a4
eualexdev <eujosealexsandro@gmail.com>
2021-05-05 21:37:31 -0300

    Ageitei alguns bugs

--- src/projects/create/__init__.py
+++ src/projects/create/__init__.py
@@ -1,32 +1,52 @@
-from PyQt5.QtWidgets import QWidget,QHBoxLayout
+from PyQt5.QtWidgets import QFrame, QHBoxLayout, QLabel
 import json
 
-from src.projects.create.cProject import UIFormProjects
+from src.projects.create.recentProjects import RecentProjects
+
 from src.configs.files import Files
 from src.configs.types import Package
 from src.configs.funcs import ReadConfigs
 
-class UICreateProject(QWidget):
+class UICreateProject(QFrame):
     def __init__(self,parent=None):
         super().__init__()
-        self.parent:QWidget = parent
+        self.parent:QFrame = parent
+
         self.jsonConfigs = ReadConfigs()
 
-        self.Configures()
+
+        self.Configure()
+        self.Widgets()
         self.ConfiguresStyles()
 
-    
+    def Configure(self):pass
+        # self.setMaximumSize(800,600)
+        # self.setMinimumSize(800,600)
 
-    def Configures(self):
-        self._uiFormProjects = UIFormProjects(self)
+    def Widgets(self):
+        self.Separators()
+        self.SetWidgets()
 
-        hbox = QHBoxLayout()
-        hbox.setSpacing(0)
-        hbox.addWidget(self._uiFormProjects)
-        self.setLayout(hbox)
+    def Separators(self):
+        self.hboxSep = QHBoxLayout()
+        self._sep1 = QFrame()
+        self._sep2 = QFrame()
+        self.hboxSep.setContentsMargins(0,0,0,0)
+        self.hboxSep.setSpacing(0)
+        self.hboxSep.addWidget(self._sep1)
+        self.hboxSep.addWidget(self._sep2)
+        self.setLayout(self.hboxSep)
+
+    def SetWidgets(self):
+        self._recentProjects = RecentProjects(self._sep1)
+        
 
     def ConfiguresStyles(self):
         theme = self.jsonConfigs["theme"]
         Coloring = json.loads(Files.Read(Package.editorThemeLocal+"/"+theme+".json"))["IdeColor"]
 
-        self._uiFormProjects.ConfiguresStyles(Coloring)
+        self.setStyleSheet(f"""background-color:{Coloring["firstColor"]};""")
+        self._sep1.setStyleSheet(f"""background-color:{Coloring["firstColor"]};""")
+        self._sep2.setStyleSheet(f"""background-color:{Coloring["firstColor"]};""")
+
+        self._recentProjects.ConfigureStyles(Coloring)
---
+++ src/projects/create/recentProjects.py
@@ -0,0 +1,34 @@
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QFrame, QLabel
+
+class RecentProjects(QFrame):
+    def __init__(self,parent=None) -> None:
+        self.parent:QFrame = parent
+        super().__init__(parent)
+
+        self.Configures()
+        self.Widegts()
+
+    def Widegts(self):
+        self.nameIde()
+
+
+    def nameIde(self):
+        self._nameIde = QLabel(self)
+        self._nameIde.setText("    ElegantIDE 2021")
+
+        font = QFont()
+        font.setFamily("Segoe Print")
+        font.setPointSize(22)
+        font.setBold(True)
+
+        self._nameIde.setFont(font)
+        self._nameIde.setGeometry(0,0,400,80)
+
+    def Configures(self):
+        self.setMaximumSize(400,600)
+        self.setMinimumSize(400,600)
+
+    def ConfigureStyles(self,Coloring):
+        self.setStyleSheet(f"""background-color:{Coloring["firstColor"]};""")
+        self._nameIde.setStyleSheet(f"""color:{Coloring["secondColor"]};""")
